6747c85cb960cd91d590015c4223212d0b54a01a
Samuel Nakazawa <samuelnakazawa895@gmail.com>
2025-03-20 23:02:53 -0300

    feat: creating body, header and footer

---
+++ src/components/ui/text-hover-effect.tsx
@@ -0,0 +1,114 @@
+'use client';
+import React, { useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+
+export const TextHoverEffect = ({
+  text,
+  duration,
+}: {
+  text: string;
+  duration?: number;
+  automatic?: boolean;
+}) => {
+  const svgRef = useRef<SVGSVGElement>(null);
+  const [hovered, setHovered] = useState(false);
+  const [maskPosition, setMaskPosition] = useState({ cx: '50%', cy: '50%' });
+
+  const handleMouseMove = (e: React.MouseEvent<SVGSVGElement>) => {
+    if (svgRef.current) {
+      const svgRect = svgRef.current.getBoundingClientRect();
+      const cxPercentage = ((e.clientX - svgRect.left) / svgRect.width) * 100;
+      const cyPercentage = ((e.clientY - svgRect.top) / svgRect.height) * 100;
+      setMaskPosition({
+        cx: `${cxPercentage}%`,
+        cy: `${cyPercentage}%`,
+      });
+    }
+  };
+
+  return (
+    <svg
+      ref={svgRef}
+      width="4em"
+      height="4em"
+      viewBox="0 0 100 50"
+      xmlns="http://www.w3.org/2000/svg"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onMouseMove={handleMouseMove}
+      className="select-none"
+      style={{ display: 'inline-block', cursor: 'pointer' }}
+    >
+      <defs>
+        <linearGradient id="textGradient" gradientUnits="userSpaceOnUse" cx="50%" cy="50%" r="25%">
+          {hovered && (
+            <>
+              <stop offset="0%" stopColor="#eab308" />
+              <stop offset="25%" stopColor="#ef4444" />
+              <stop offset="50%" stopColor="#3b82f6" />
+              <stop offset="75%" stopColor="#06b6d4" />
+              <stop offset="100%" stopColor="#8b5cf6" />
+            </>
+          )}
+        </linearGradient>
+
+        <motion.radialGradient
+          id="revealMask"
+          gradientUnits="userSpaceOnUse"
+          r="20%"
+          initial={{ cx: '50%', cy: '50%' }}
+          animate={maskPosition}
+          transition={{ duration: duration ?? 0, ease: 'easeOut' }}
+        >
+          <stop offset="0%" stopColor="white" />
+          <stop offset="100%" stopColor="black" />
+        </motion.radialGradient>
+        <mask id="textMask">
+          <rect x="0" y="0" width="100%" height="100%" fill="url(#revealMask)" />
+        </mask>
+      </defs>
+      <text
+        x="50%"
+        y="50%"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        strokeWidth="0.3"
+        className="fill-transparent stroke-neutral-200 font-[helvetica] text-lg font-bold dark:stroke-neutral-800" // Tamanho da fonte reduzido
+        style={{ opacity: hovered ? 0.7 : 0 }}
+      >
+        {text}
+      </text>
+      <motion.text
+        x="50%"
+        y="50%"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        strokeWidth="0.3"
+        className="fill-transparent stroke-neutral-200 font-[helvetica] text-lg font-bold dark:stroke-neutral-800" // Tamanho da fonte reduzido
+        initial={{ strokeDashoffset: 1000, strokeDasharray: 1000 }}
+        animate={{
+          strokeDashoffset: 0,
+          strokeDasharray: 1000,
+        }}
+        transition={{
+          duration: 4,
+          ease: 'easeInOut',
+        }}
+      >
+        {text}
+      </motion.text>
+      <text
+        x="50%"
+        y="50%"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        stroke="url(#textGradient)"
+        strokeWidth="0.3"
+        mask="url(#textMask)"
+        className="fill-transparent font-[helvetica] text-lg font-bold" // Tamanho da fonte reduzido
+      >
+        {text}
+      </text>
+    </svg>
+  );
+};
