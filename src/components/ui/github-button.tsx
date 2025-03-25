78e8d9130dd7154803d622e24c1d7107451a347a
Samuel Nakazawa <samuelnakazawa895@gmail.com>
2025-03-25 01:53:47 -0300

    fix(github-button): fixing github button bg color (commiting all aparted because im tired asf)

--- src/components/ui/github-button.tsx
+++ src/components/ui/github-button.tsx
@@ -15,7 +15,7 @@ export const GitHubButton = () => {
       href={'https://github.com/samuelnakazawa/naka.dev'}
       target="_blank"
       rel="noopener noreferrer"
-      className="inline-flex items-center justify-center px-4 py-3 bg-black text-white font-semibold rounded-lg border border-white transition-all duration-500 ease-in-out hover:border-transparent hover:bg-gradient-to-tr from-[#c95bf5] to-[#8a2be2] hover:shadow-lg hover:shadow-[#c95bf5]/50 ml-[20px] relative"
+      className="inline-flex items-center justify-center px-4 py-3 bg-transparent text-white font-semibold rounded-lg border border-white transition-all duration-500 ease-in-out hover:border-transparent hover:bg-gradient-to-tr from-[#c95bf5] to-[#8a2be2] hover:shadow-lg hover:shadow-[#c95bf5]/50 ml-[20px] relative"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       variants={buttonVariants}
